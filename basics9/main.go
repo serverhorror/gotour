ef8d97b62e8af18e56934b383b433ca5341b8c1f
serverhorror <serverhorror@users.noreply.github.com>
2023-12-30 20:11:55 +0100

    show type inference for vars with initializers

--- basics9/main.go
+++ basics9/main.go
@@ -7,4 +7,6 @@ var i, j int = 1, 2
 func main() {
 	var c, python, java = true, false, "no!"
 	fmt.Println(i, j, c, python, java)
+	fmt.Printf("i: %T, j: %T, c: %T, python: %T, java: %T\n",
+		i, j, c, python, java)
 }
