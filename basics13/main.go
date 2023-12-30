70895a5cbaf7ef6d50fedcb589ae90ef4f637afa
serverhorror <serverhorror@users.noreply.github.com>
2023-12-30 20:24:02 +0100

    type conversion does actually work in Go!!
    Captain obvious flew by!

--- basics13/main.go
+++ basics13/main.go
@@ -10,4 +10,6 @@ func main() {
 	var f float64 = math.Sqrt(float64(x*x + y*y))
 	var z uint = uint(f)
 	fmt.Println(x, y, z)
+	fmt.Printf("x: %v(%T), y: %v(%T), f: %v(%T), z: %v(%T)", x, x, y, y, f, f, z, z)
+	fmt.Printf("x: %v(%T), y: %v(%T), z: %v(%T)", x, x, y, y, z, z)
 }
